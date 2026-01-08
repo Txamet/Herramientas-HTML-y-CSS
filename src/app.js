3c0e9c60cc27893e1d3cb22e2bd9d00dbaf5fce7
Jaume Blanco González <jaumeblancog@gmail.com>
2026-01-08 19:59:41 +0100

    Downloaded fonts and icons instead of cdn use

--- src/app.js
+++ src/app.js
@@ -1,2 +1,3 @@
 import "./assets/scripts/top-button.js";
 import "./assets/scripts/actual-page.js";
+import "./assets/scripts/icons.js";
---
+++ src/assets/scripts/icons.js
@@ -0,0 +1,6 @@
+import { library, dom } from "@fortawesome/fontawesome-svg-core";
+import { faCalendarDays, faBook, faCloudSun, faPersonHiking, faMapLocationDot } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faCalendarDays, faBook, faCloudSun, faPersonHiking, faMapLocationDot);
+
+dom.watch();
